8ec53b0384780c3750911af11dfa925f80d9bbae
Mohamed Elasfoury <mohamed.elasfoury@epfl.ch>
2021-12-14 11:09:31 +0100

    Generate training data

---
+++ code/preprocess_fasttext.py
@@ -0,0 +1,103 @@
+import numpy as np
+from ekphrasis.classes.preprocessor import TextPreProcessor
+from ekphrasis.classes.tokenizer import SocialTokenizer
+from ekphrasis.dicts.emoticons import emoticons
+from nltk.corpus import stopwords
+import string
+from tqdm import tqdm
+import fasttext
+
+
+POS_PATH = "../Twitter_DataSet/train_pos_full_nodup.txt"
+NEG_PATH = "../Twitter_DataSet/train_neg_full_nodup.txt"
+
+pos_tweets = []
+neg_tweets = []
+with open(POS_PATH,"r") as f:
+    pos_tweets = f.readlines()
+    f.close()
+with open(NEG_PATH, "r") as f:
+    neg_tweets = f.readlines()
+    f.close()
+print(len(pos_tweets))
+print(len(neg_tweets))
+
+def is_in_tag(word):
+    if word.startswith('<') and word.endswith('>'):
+        return True
+    return False
+
+def clean_tweets(tweets):
+    # process words using text processor 
+    cleaned = [text_processor.pre_process_doc(s) for s in tqdm(tweets)]
+    # remove words with tags <>
+    cleaned = [list(filter(lambda word: not is_in_tag(word), tweet)) for tweet in tqdm(cleaned)]
+    # remove punctuation
+    cleaned =  [list(filter(lambda word: word not in list(string.punctuation), tweet)) for tweet in tqdm(cleaned)]
+    # remove stop words 
+    stop_words = list(set(stopwords.words('english')))
+    cleaned_tweets =  [list(filter(lambda word: word not in stop_words, tweet)) for tweet in tqdm(cleaned)]
+    return cleaned_tweets
+
+
+text_processor = TextPreProcessor(
+    # terms that will be normalized
+    normalize=['url', 'email', 'percent', 'money', 'phone', 'user',
+        'time', 'url', 'date', 'number'],
+    # terms that will be annotated
+    annotate={"hashtag", "allcaps", "elongated", "repeated",
+        'emphasis', 'censored'},
+    fix_html=True,  # fix HTML tokens
+    
+    # corpus from which the word statistics are going to be used 
+    # for word segmentation 
+    segmenter="twitter", 
+    
+    # corpus from which the word statistics are going to be used 
+    # for spell correction
+    corrector="twitter", 
+    
+    unpack_hashtags=True,  # perform word segmentation on hashtags
+    unpack_contractions=True,  # Unpack contractions (can't -> can not)
+    spell_correct_elong=True,  # spell correction for elongated words
+    
+    # select a tokenizer. You can use SocialTokenizer, or pass your own
+    # the tokenizer, should take as input a string and return a list of tokens
+    tokenizer=SocialTokenizer(lowercase=True).tokenize,
+    
+    # list of dictionaries, for replacing tokens extracted from the text,
+    # with other expressions. You can pass more than one dictionaries.
+    dicts=[emoticons]
+)
+
+
+neg_tweets_cleaned = clean_tweets(neg_tweets)
+pos_tweets_cleaned = clean_tweets(pos_tweets)
+
+print(len(pos_tweets_cleaned))
+print(len(neg_tweets_cleaned))
+
+ft = fasttext.load_model('cc.en.300.bin')
+ft.get_dimension()
+
+
+def get_word_emb(tweet):
+    avg_vect = np.zeros(300)
+    for word in tweet:
+        avg_vect += ft.get_word_vector(word)
+    return avg_vect / 300
+
+# [1, 0] encodes :( tweet
+# [0, 1] encodes :) tweet
+training_data = []
+for tweet in tqdm(neg_tweets_cleaned):
+    emb = get_word_emb(tweet)
+    training_data.append([np.array(emb), np.array([1.0, 0.0])])
+
+for tweet in tqdm(pos_tweets_cleaned):
+    emb = get_word_emb(tweet)
+    training_data.append([np.array(emb),np.array([0.0, 1.0])])
+
+print("Shuffiling and saving training_data")
+np.random.shuffle(training_data)
+np.save("training_data.npy", training_data)
